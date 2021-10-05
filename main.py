bf929f0f6754a0f857998d11690c6118198f83c7
Raz Matzliah <razm1213@gmail.com>
2021-10-05 12:08:43 +0300

    imported necessary modules to main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import pygame
+import random
+import os.path
+from classes.Player import Player
+from classes.Enemy import Enemy
+from classes.Bullet import Bullet
+#Initiallize the pygame:
+pygame.init()
