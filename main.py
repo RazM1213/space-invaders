f724e73bdbc1738f8fcd86bfcf86ec060ad62fd5
Raz Matzliah <razm1213@gmail.com>
2021-10-05 12:10:02 +0300

    added setup variables

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ from classes.Enemy import Enemy
 from classes.Bullet import Bullet
 #Initiallize the pygame:
 pygame.init()
+
+caption = pygame.display.set_caption('Space Invaders')
+icon = pygame.image.load('images/spaceship.png')
+bg = pygame.image.load('images/space.jpg')
+font = pygame.font.SysFont('comicsans',50,True,True)
+score = 0
+pygame.display.set_icon(icon)
