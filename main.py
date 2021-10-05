a9b2dabe9310d24bca5102101fef44178b5189df
Raz Matzliah <razm1213@gmail.com>
2021-10-05 12:11:47 +0300

    added redraw_game_window func

--- main.py
+++ main.py
@@ -19,3 +19,19 @@ alien = Enemy(random.randint(0,736),random.randint(50,150),64,64,720)
 bullets = []
 aliens = [alien]
 shootLoop = 0
+
+def redraw_game_window():
+    screen = pygame.display.set_mode((800, 600))
+    screen.blit(bg, (0, -200))
+    player.draw(screen)
+    for alien in aliens:
+        alien.draw(screen)
+        alien.move()
+
+    for bullet in bullets:
+        bullet.draw(screen)
+
+    text1 = font.render('Score: ' + str(score),1,(0,0,0))
+    screen.blit(text1, (550,20))
+
+    pygame.display.update()
